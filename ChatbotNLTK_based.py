c99ac43dbef1f6e70e69930b50d6c7e7fe214b6e
Gregory Meyer <greg.b.meyer@gmail.com>
2019-06-12 13:35:20 -0500

    updated for a changeable similarity cutoff
    also added in index back off to get the proper response to the matched statement

--- ChatbotNLTK_based.py
+++ ChatbotNLTK_based.py
@@ -2,8 +2,10 @@ import nltk
 import numpy as np
 import random
 import string
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-f=open('chatbot.txt','r',errors = 'ignore')
+f=open('Desktop\Data\TacoStandCorpus.txt','r',errors = 'ignore')
 raw=f.read()
 raw=raw.lower()# converts to lowercase
 nltk.download('punkt') # first-time use only
@@ -11,38 +13,53 @@ nltk.download('wordnet') # first-time use only
 sent_tokens = nltk.sent_tokenize(raw)# converts to list of sentences 
 word_tokens = nltk.word_tokenize(raw)# converts to list of words
 
+GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up","hey",)
+GREETING_RESPONSES = ["hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"]
+similarityCutOff=0.3
+
 lemmer = nltk.stem.WordNetLemmatizer()
+remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+
 def LemTokens(tokens):
     return [lemmer.lemmatize(token) for token in tokens]
-remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+
 def LemNormalize(text):
     return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
 
-GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up","hey",)
-GREETING_RESPONSES = ["hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"]
 def greeting(sentence):
- 
     for word in sentence.split():
         if word.lower() in GREETING_INPUTS:
             return random.choice(GREETING_RESPONSES)
 
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
 def response(user_response):
     robo_response=''
     sent_tokens.append(user_response)
     TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
     tfidf = TfidfVec.fit_transform(sent_tokens)
     vals = cosine_similarity(tfidf[-1], tfidf)
+    #print("cosine similarity vals")
+    #print(vals)
+    #get the id of the best response, [-1]=the actual typed in phrase so when sorted -2 is the highest match
     idx=vals.argsort()[0][-2]
+    idx1=vals.argsort()[0][-3]
+    idx2=vals.argsort()[0][-4]
+    #however, we have to step back one more from the indices of the top matches to find the proper response to the match
+    idList=[idx-1,idx1-1,idx2-1]
+    #and we choose randomly from the top 3 matches to respond with
+    idT=random.choice(idList)
+    #we have to get our response similarity list into a form to sort 
+    #based on the actual cosine similarity values and not ids
     flat = vals.flatten()
     flat.sort()
     req_tfidf = flat[-2]
-    if(req_tfidf==0):
-        robo_response=robo_response+"Unable to grok that dude!"
+    #print("flat[-2] value")
+    #print(req_tfidf)
+    if(req_tfidf<similarityCutOff):
+        robo_response="Thing does not understand. Please rephrase."
         return robo_response
     else:
-        robo_response = robo_response+sent_tokens[idx]
+        #this is how we reference the matched response
+        robo_response = sent_tokens[idT]
         return robo_response
 
 flag=True
@@ -64,4 +81,3 @@ while(flag==True):
     else:
         flag=False
         print("ChatBot: Ciao!")
-
